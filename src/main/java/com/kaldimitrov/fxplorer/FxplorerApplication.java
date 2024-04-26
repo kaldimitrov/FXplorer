82655995ebd445ce0c7862c5853f8ae626326a22
Kaloyan <igntoxicshadow@gmail.com>
2024-04-26 21:26:30 +0300

    Initialized Spring Boot project with dependencies

---
+++ src/main/java/com/kaldimitrov/fxplorer/FxplorerApplication.java
@@ -0,0 +1,13 @@
+package com.kaldimitrov.fxplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxplorerApplication.class, args);
+	}
+
+}
